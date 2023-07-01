4de18de99ec20eb314b117abf3045de3c3a1f7ad
aki-iwanaga <akihiro_iwanaga_aa@mail.toyota.co.jp>
2023-07-02 08:31:33 +0900

    first commit

---
+++ server/src/main/kotlin/com/example/tutorialTodoapp/TutorialTodoappApplication.kt
@@ -0,0 +1,11 @@
+package com.example.tutorialTodoapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TutorialTodoappApplication
+
+fun main(args: Array<String>) {
+	runApplication<TutorialTodoappApplication>(*args)
+}
---
+++ server/src/test/kotlin/com/example/tutorialTodoapp/TutorialTodoappApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.tutorialTodoapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TutorialTodoappApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
